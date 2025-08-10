1e3f5ab1d8d3343084e3cc958486ce3955a8cb9c
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-08-10 14:18:39 -0700

    chore: remove tauri shell plugin from Cargo.lock and Cargo.toml

--- apps/whispering/src-tauri/src/lib.rs
+++ apps/whispering/src-tauri/src/lib.rs
@@ -24,7 +24,6 @@ pub fn run() {
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .manage(AppData::new());
